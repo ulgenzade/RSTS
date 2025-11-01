f3b213db2e3bb55918af1c6321a627de89364ebc
Alper Topal <alper.topal.2006@gmail.com>
2025-11-01 20:39:35 +0300

    Revert "fix"
    This reverts commit c9f937e9f15186b59e8c243d975800de5638f6d9.

--- RestoranOtomasyon/frmSiparisEkrani.Designer.cs
+++ RestoranOtomasyon/frmSiparisEkrani.Designer.cs
@@ -29,15 +29,15 @@
         private void InitializeComponent()
         {
             this.flowMasalar = new System.Windows.Forms.FlowLayoutPanel();
-            this.btnOrnekMasa = new System.Windows.Forms.Button();
             this.panelSiparis = new System.Windows.Forms.Panel();
-            this.btnOdeme = new System.Windows.Forms.Button();
-            this.lblGarsonAdi = new System.Windows.Forms.Label();
-            this.btnOnayla = new System.Windows.Forms.Button();
-            this.btnVazgec = new System.Windows.Forms.Button();
-            this.panelSiparisListesi = new System.Windows.Forms.Panel();
-            this.txtArama = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.txtArama = new System.Windows.Forms.TextBox();
+            this.panelSiparisListesi = new System.Windows.Forms.Panel();
+            this.btnVazgec = new System.Windows.Forms.Button();
+            this.btnOnayla = new System.Windows.Forms.Button();
+            this.lblGarsonAdi = new System.Windows.Forms.Label();
+            this.btnOdeme = new System.Windows.Forms.Button();
+            this.btnOrnekMasa = new System.Windows.Forms.Button();
             this.flowMasalar.SuspendLayout();
             this.panelSiparis.SuspendLayout();
             this.SuspendLayout();
@@ -52,15 +52,6 @@
             this.flowMasalar.Size = new System.Drawing.Size(200, 577);
             this.flowMasalar.TabIndex = 0;
             this.flowMasalar.Paint += new System.Windows.Forms.PaintEventHandler(this.flowMasalar_Paint);
-            // 
-            // btnOrnekMasa
-            // 
-            this.btnOrnekMasa.Location = new System.Drawing.Point(3, 3);
-            this.btnOrnekMasa.Name = "btnOrnekMasa";
-            this.btnOrnekMasa.Size = new System.Drawing.Size(196, 115);
-            this.btnOrnekMasa.TabIndex = 0;
-            this.btnOrnekMasa.Text = "MASA-1";
-            this.btnOrnekMasa.UseVisualStyleBackColor = true;
             // 
             // panelSiparis
             // 
@@ -79,15 +70,49 @@
             this.panelSiparis.Visible = false;
             this.panelSiparis.Paint += new System.Windows.Forms.PaintEventHandler(this.panelSiparis_Paint);
             // 
-            // btnOdeme
+            // label1
             // 
-            this.btnOdeme.Location = new System.Drawing.Point(666, 517);
-            this.btnOdeme.Name = "btnOdeme";
-            this.btnOdeme.Size = new System.Drawing.Size(254, 23);
-            this.btnOdeme.TabIndex = 6;
-            this.btnOdeme.Text = "ÖDE";
-            this.btnOdeme.UseVisualStyleBackColor = true;
-            this.btnOdeme.Click += new System.EventHandler(this.btnOdeme_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(497, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SİPARİŞLER";
+            // 
+            // txtArama
+            // 
+            this.txtArama.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtArama.Location = new System.Drawing.Point(0, 0);
+            this.txtArama.Name = "txtArama";
+            this.txtArama.Size = new System.Drawing.Size(932, 22);
+            this.txtArama.TabIndex = 1;
+            // 
+            // panelSiparisListesi
+            // 
+            this.panelSiparisListesi.AutoScroll = true;
+            this.panelSiparisListesi.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelSiparisListesi.Location = new System.Drawing.Point(0, 22);
+            this.panelSiparisListesi.Name = "panelSiparisListesi";
+            this.panelSiparisListesi.Size = new System.Drawing.Size(932, 100);
+            this.panelSiparisListesi.TabIndex = 2;
+            // 
+            // btnVazgec
+            // 
+            this.btnVazgec.Location = new System.Drawing.Point(666, 488);
+            this.btnVazgec.Name = "btnVazgec";
+            this.btnVazgec.Size = new System.Drawing.Size(124, 23);
+            this.btnVazgec.TabIndex = 3;
+            this.btnVazgec.Text = "VAZGEÇ";
+            this.btnVazgec.UseVisualStyleBackColor = true;
+            // 
+            // btnOnayla
+            // 
+            this.btnOnayla.Location = new System.Drawing.Point(796, 488);
+            this.btnOnayla.Name = "btnOnayla";
+            this.btnOnayla.Size = new System.Drawing.Size(124, 23);
+            this.btnOnayla.TabIndex = 4;
+            this.btnOnayla.Text = "ONAYLA";
+            this.btnOnayla.UseVisualStyleBackColor = true;
             // 
             // lblGarsonAdi
             // 
@@ -99,49 +124,24 @@
             this.lblGarsonAdi.Text = "label2";
             this.lblGarsonAdi.Click += new System.EventHandler(this.lblGarsonAdi_Click);
             // 
-            // btnOnayla
+            // btnOdeme
             // 
-            this.btnOnayla.Location = new System.Drawing.Point(796, 488);
-            this.btnOnayla.Name = "btnOnayla";
-            this.btnOnayla.Size = new System.Drawing.Size(124, 23);
-            this.btnOnayla.TabIndex = 4;
-            this.btnOnayla.Text = "ONAYLA";
-            this.btnOnayla.UseVisualStyleBackColor = true;
+            this.btnOdeme.Location = new System.Drawing.Point(666, 517);
+            this.btnOdeme.Name = "btnOdeme";
+            this.btnOdeme.Size = new System.Drawing.Size(254, 23);
+            this.btnOdeme.TabIndex = 6;
+            this.btnOdeme.Text = "ÖDE";
+            this.btnOdeme.UseVisualStyleBackColor = true;
+            this.btnOdeme.Click += new System.EventHandler(this.btnOdeme_Click);
             // 
-            // btnVazgec
+            // btnOrnekMasa
             // 
-            this.btnVazgec.Location = new System.Drawing.Point(666, 488);
-            this.btnVazgec.Name = "btnVazgec";
-            this.btnVazgec.Size = new System.Drawing.Size(124, 23);
-            this.btnVazgec.TabIndex = 3;
-            this.btnVazgec.Text = "VAZGEÇ";
-            this.btnVazgec.UseVisualStyleBackColor = true;
-            // 
-            // panelSiparisListesi
-            // 
-            this.panelSiparisListesi.AutoScroll = true;
-            this.panelSiparisListesi.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelSiparisListesi.Location = new System.Drawing.Point(0, 22);
-            this.panelSiparisListesi.Name = "panelSiparisListesi";
-            this.panelSiparisListesi.Size = new System.Drawing.Size(932, 100);
-            this.panelSiparisListesi.TabIndex = 2;
-            // 
-            // txtArama
-            // 
-            this.txtArama.Dock = System.Windows.Forms.DockStyle.Top;
-            this.txtArama.Location = new System.Drawing.Point(0, 0);
-            this.txtArama.Name = "txtArama";
-            this.txtArama.Size = new System.Drawing.Size(932, 22);
-            this.txtArama.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(497, 71);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "SİPARİŞLER";
+            this.btnOrnekMasa.Location = new System.Drawing.Point(3, 3);
+            this.btnOrnekMasa.Name = "btnOrnekMasa";
+            this.btnOrnekMasa.Size = new System.Drawing.Size(196, 115);
+            this.btnOrnekMasa.TabIndex = 0;
+            this.btnOrnekMasa.Text = "MASA-1";
+            this.btnOrnekMasa.UseVisualStyleBackColor = true;
             // 
             // frmSiparisEkrani
             // 
@@ -150,6 +150,7 @@
             this.ClientSize = new System.Drawing.Size(1132, 577);
             this.Controls.Add(this.panelSiparis);
             this.Controls.Add(this.flowMasalar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "frmSiparisEkrani";
             this.Text = "frmSiparisEkrani";
             this.Load += new System.EventHandler(this.frmSiparisEkrani_Load);
