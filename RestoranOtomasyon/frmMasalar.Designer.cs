ee4b55a2707a1c1e2e279395c0e1e37e5bebbfbe
ulgenzade <atoksoz67@gmail.com>
2025-11-01 15:46:25 +0300

    Sipariş Takip Ekranı Taslak#2

---
+++ RestoranOtomasyon/frmMasalar.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace RestoranOtomasyon
+{
+    partial class frmMasalar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowMasalar = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flowMasalar
+            // 
+            this.flowMasalar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowMasalar.Location = new System.Drawing.Point(0, 0);
+            this.flowMasalar.Name = "flowMasalar";
+            this.flowMasalar.Padding = new System.Windows.Forms.Padding(10);
+            this.flowMasalar.Size = new System.Drawing.Size(800, 450);
+            this.flowMasalar.TabIndex = 0;
+            this.flowMasalar.Paint += new System.Windows.Forms.PaintEventHandler(this.flowMasalar_Paint);
+            // 
+            // frmMasalar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowMasalar);
+            this.Name = "frmMasalar";
+            this.Text = "frmMasalar";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowMasalar;
+    }
+}
