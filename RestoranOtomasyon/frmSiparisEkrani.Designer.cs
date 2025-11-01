907e0681e4adea3ceb1dff4d12699c640fff843f
ulgenzade <atoksoz67@gmail.com>
2025-11-01 15:04:04 +0300

    Sipariş Ekranı Taslak#1

--- RestoranOtomasyon/frmSiparisEkrani.Designer.cs
+++ RestoranOtomasyon/frmSiparisEkrani.Designer.cs
@@ -28,12 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.flowMasalar = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnOrnekMasa = new System.Windows.Forms.Button();
+            this.panelSiparis = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtArama = new System.Windows.Forms.TextBox();
+            this.panelSiparisListesi = new System.Windows.Forms.Panel();
+            this.btnVazgec = new System.Windows.Forms.Button();
+            this.btnOnayla = new System.Windows.Forms.Button();
+            this.lblGarsonAdi = new System.Windows.Forms.Label();
+            this.flowMasalar.SuspendLayout();
+            this.panelSiparis.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowMasalar
+            // 
+            this.flowMasalar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowMasalar.Controls.Add(this.btnOrnekMasa);
+            this.flowMasalar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowMasalar.Location = new System.Drawing.Point(0, 0);
+            this.flowMasalar.Name = "flowMasalar";
+            this.flowMasalar.Size = new System.Drawing.Size(200, 577);
+            this.flowMasalar.TabIndex = 0;
+            // 
+            // btnOrnekMasa
+            // 
+            this.btnOrnekMasa.Location = new System.Drawing.Point(3, 3);
+            this.btnOrnekMasa.Name = "btnOrnekMasa";
+            this.btnOrnekMasa.Size = new System.Drawing.Size(150, 100);
+            this.btnOrnekMasa.TabIndex = 0;
+            this.btnOrnekMasa.Text = "MASA - 1";
+            this.btnOrnekMasa.UseVisualStyleBackColor = true;
+            this.btnOrnekMasa.Click += new System.EventHandler(this.btnOrnekMasa_Click);
+            // 
+            // panelSiparis
+            // 
+            this.panelSiparis.Controls.Add(this.lblGarsonAdi);
+            this.panelSiparis.Controls.Add(this.btnOnayla);
+            this.panelSiparis.Controls.Add(this.btnVazgec);
+            this.panelSiparis.Controls.Add(this.panelSiparisListesi);
+            this.panelSiparis.Controls.Add(this.txtArama);
+            this.panelSiparis.Controls.Add(this.label1);
+            this.panelSiparis.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelSiparis.Location = new System.Drawing.Point(200, 0);
+            this.panelSiparis.Name = "panelSiparis";
+            this.panelSiparis.Size = new System.Drawing.Size(932, 577);
+            this.panelSiparis.TabIndex = 1;
+            this.panelSiparis.Visible = false;
+            this.panelSiparis.Paint += new System.Windows.Forms.PaintEventHandler(this.panelSiparis_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(497, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SİPARİŞLER";
+            // 
+            // txtArama
+            // 
+            this.txtArama.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtArama.Location = new System.Drawing.Point(0, 0);
+            this.txtArama.Name = "txtArama";
+            this.txtArama.Size = new System.Drawing.Size(932, 22);
+            this.txtArama.TabIndex = 1;
+            // 
+            // panelSiparisListesi
+            // 
+            this.panelSiparisListesi.AutoScroll = true;
+            this.panelSiparisListesi.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelSiparisListesi.Location = new System.Drawing.Point(0, 22);
+            this.panelSiparisListesi.Name = "panelSiparisListesi";
+            this.panelSiparisListesi.Size = new System.Drawing.Size(932, 100);
+            this.panelSiparisListesi.TabIndex = 2;
+            // 
+            // btnVazgec
+            // 
+            this.btnVazgec.Location = new System.Drawing.Point(747, 526);
+            this.btnVazgec.Name = "btnVazgec";
+            this.btnVazgec.Size = new System.Drawing.Size(75, 23);
+            this.btnVazgec.TabIndex = 3;
+            this.btnVazgec.Text = "VAZGEÇ";
+            this.btnVazgec.UseVisualStyleBackColor = true;
+            // 
+            // btnOnayla
+            // 
+            this.btnOnayla.Location = new System.Drawing.Point(845, 526);
+            this.btnOnayla.Name = "btnOnayla";
+            this.btnOnayla.Size = new System.Drawing.Size(75, 23);
+            this.btnOnayla.TabIndex = 4;
+            this.btnOnayla.Text = "ONAYLA";
+            this.btnOnayla.UseVisualStyleBackColor = true;
+            // 
+            // lblGarsonAdi
+            // 
+            this.lblGarsonAdi.AutoSize = true;
+            this.lblGarsonAdi.Location = new System.Drawing.Point(19, 533);
+            this.lblGarsonAdi.Name = "lblGarsonAdi";
+            this.lblGarsonAdi.Size = new System.Drawing.Size(44, 16);
+            this.lblGarsonAdi.TabIndex = 5;
+            this.lblGarsonAdi.Text = "label2";
+            this.lblGarsonAdi.Click += new System.EventHandler(this.lblGarsonAdi_Click);
+            // 
+            // frmSiparisEkrani
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1132, 577);
+            this.Controls.Add(this.panelSiparis);
+            this.Controls.Add(this.flowMasalar);
+            this.Name = "frmSiparisEkrani";
             this.Text = "frmSiparisEkrani";
+            this.Load += new System.EventHandler(this.frmSiparisEkrani_Load);
+            this.flowMasalar.ResumeLayout(false);
+            this.panelSiparis.ResumeLayout(false);
+            this.panelSiparis.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowMasalar;
+        private System.Windows.Forms.Panel panelSiparis;
+        private System.Windows.Forms.Button btnOrnekMasa;
+        private System.Windows.Forms.TextBox txtArama;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblGarsonAdi;
+        private System.Windows.Forms.Button btnOnayla;
+        private System.Windows.Forms.Button btnVazgec;
+        private System.Windows.Forms.Panel panelSiparisListesi;
     }
 }
--- RestoranOtomasyon/frmSiparisEkrani.cs
+++ RestoranOtomasyon/frmSiparisEkrani.cs
@@ -16,5 +16,108 @@ namespace RestoranOtomasyon
         {
             InitializeComponent();
         }
+
+        // FORM İLK AÇILDIĞINDA BU METOT ÇALIŞIR
+        private void frmSiparisEkrani_Load(object sender, EventArgs e)
+        {
+            // Sol tarafa 1'den 6'ya kadar masa butonlarını oluşturalım.
+            // Bu kısım daha sonra Alper'in veritabanından masaları çeken koduyla değiştirilecek.
+            for (int i = 1; i <= 6; i++)
+            {
+                // Yeni bir buton nesnesi yarat
+                Button masaButonu = new Button();
+
+                // Butonun özelliklerini ayarla
+                masaButonu.Text = "MASA - " + i;
+                masaButonu.Name = "btnMasa" + i;
+                masaButonu.Size = new Size(150, 100);       // Butonun boyutu
+                masaButonu.Margin = new Padding(10);         // Butonlar arası boşluk
+                masaButonu.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+                masaButonu.Tag = i;                          // Butonun hangi masa olduğunu (ID'sini) saklamak için gizli bir etiket. Bu çok önemlidir.
+
+                // Bu yeni oluşturulan butona tıklandığında hangi metodun çalışacağını belirtiyoruz.
+                // Bütün masa butonları tıklandığında AŞAĞIDAKİ AYNI metot çalışacak.
+                masaButonu.Click += new EventHandler(MasaButonu_Click);
+
+                // Hazırlanan butonu sol panelimize (flowMasalar) ekle
+                flowMasalar.Controls.Add(masaButonu);
+            }
+        }
+
+        // YUKARIDA OLUŞTURULAN MASA BUTONLARINDAN HERHANGİ BİRİNE TIKLANDIĞINDA BU METOT ÇALIŞIR
+        private void MasaButonu_Click(object sender, EventArgs e)
+        {
+            // 'sender' parametresi, hangi butonun tıklandığı bilgisini taşır.
+            Button tiklananMasaButonu = sender as Button;
+
+            // Güvenlik kontrolü
+            if (tiklananMasaButonu == null) return;
+
+            // Butonun Tag özelliğinde sakladığımız masa numarasını alıyoruz.
+            int masaNumarasi = Convert.ToInt32(tiklananMasaButonu.Tag);
+
+            // Sağdaki sipariş panelini görünür yapalım
+            panelSiparis.Visible = true;
+
+            // Sipariş panelinin içini önce bir temizleyelim (varsa eski siparişlerden kurtulmak için)
+            panelSiparisListesi.Controls.Clear();
+
+            // --- BU KISIM DAHA SONRA VERİTABANI KODLARIYLA DEĞİŞECEK ---
+            // Şimdilik, tıklanan masaya göre sahte sipariş verileri oluşturalım.
+            // Örneğin, 1 numaralı masanın siparişleri farklı, diğerlerinin farklı olsun.
+            List<string> urunler = new List<string>();
+            if (masaNumarasi == 1)
+            {
+                urunler.Add("Adana Kebap");
+                urunler.Add("Ayran");
+                urunler.Add("Salata");
+            }
+            else
+            {
+                urunler.Add("Çorba");
+                urunler.Add("İskender");
+                urunler.Add("Kola");
+                urunler.Add("Künefe");
+            }
+            // -------------------------------------------------------------
+
+            // Sağ tarafa, gelen her bir ürün için bir "sipariş satırı" ekleyelim
+            foreach (string urunAdi in urunler)
+            {
+                // Her bir sipariş satırı için bir Panel oluşturuyoruz. Bu, elemanları bir arada tutar.
+                Panel urunSatiriPaneli = new Panel();
+                urunSatiriPaneli.Size = new Size(panelSiparisListesi.Width - 25, 40); // Genişliği ana panele göre ayarla
+                urunSatiriPaneli.BorderStyle = BorderStyle.FixedSingle;
+                urunSatiriPaneli.Dock = DockStyle.Top; // Panellerin üst üste dizilmesini sağlar
+
+                // Panelin içine ürünün adını yazan bir Label ekleyelim
+                Label lblUrunAdi = new Label();
+                lblUrunAdi.Text = urunAdi;
+                lblUrunAdi.Location = new Point(10, 10);
+                lblUrunAdi.AutoSize = true;
+
+                // Hazırladığımız Label'ı satır paneline ekleyelim
+                urunSatiriPaneli.Controls.Add(lblUrunAdi);
+
+                // Hazırladığımız bu satır panelini de ana sipariş listesi paneline ekliyoruz
+                panelSiparisListesi.Controls.Add(urunSatiriPaneli);
+            }
+        }
+
+        // Bu metot, tasarımcı tarafından otomatik oluşturulmuştur, şimdilik boş kalabilir.
+        private void panelSiparis_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnOrnekMasa_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblGarsonAdi_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
