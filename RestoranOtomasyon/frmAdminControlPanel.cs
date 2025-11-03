b3871508e20c76954b7d4bbbf42a21864e5c2e74
Alper Topal <alper.topal.2006@gmail.com>
2025-11-03 20:00:34 +0300

    Ben birşeyler yaptım

--- RestoranOtomasyon/frmAdminControlPanel.cs
+++ RestoranOtomasyon/frmAdminControlPanel.cs
@@ -14,7 +14,7 @@ namespace RestoranOtomasyon
     {
         public frmAdminControlPanel()
         {
-            InitializeComponent();
+            
         }
 
         private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
