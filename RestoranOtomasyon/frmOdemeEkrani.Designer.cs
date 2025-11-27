4f7000c4ec8fd90f11349a2b5597f6f4cb1753e2
Yunus Emre KARAMEŞE <yunuskaramese57@gmail.com>
2025-11-27 14:51:50 +0300

    a

--- RestoranOtomasyon/frmOdemeEkrani.Designer.cs
+++ RestoranOtomasyon/frmOdemeEkrani.Designer.cs
@@ -368,7 +368,6 @@
             this.btnBitir.Type = ReaLTaiizor.Controls.MaterialButton.MaterialButtonType.Contained;
             this.btnBitir.UseAccentColor = false;
             this.btnBitir.UseVisualStyleBackColor = true;
-            this.btnBitir.Click += new System.EventHandler(this.btnBitir_Click);
             // 
             // btnHepsiniGeriAl
             // 
